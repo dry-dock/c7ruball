caa2c7f91f378083419e6ca4ecd01e529ff9b843
Ubuntu <ubuntu@ip-80-0-0-44.ec2.internal>
2018-11-16 08:16:12 +0000

    Adding files

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 
-sudo apt-get clean
-sudo mv /var/lib/apt/lists/* /tmp
-sudo mkdir -p /var/lib/apt/lists/partial
-sudo apt-get clean
-sudo apt-get update
+#sudo apt-get clean
+#sudo mv /var/lib/apt/lists/* /tmp
+#sudo mkdir -p /var/lib/apt/lists/partial
+#sudo apt-get clean
+#sudo apt-get update
 
-echo '===== Installing libgmp3-dev package ====='
-sudo apt-get install libgmp3-dev
-sudo apt-get update
+#echo '===== Installing libgmp3-dev package ====='
+#sudo apt-get install libgmp3-dev
+sudo yum update
 
 echo "================= Updating global rvm packages ==================="
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
@@ -16,15 +16,15 @@ curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 echo '[[ -s /etc/profile.d/rvm.sh ]] && source /etc/profile.d/rvm.sh' >> /etc/drydock/.env
 [[ -s /etc/profile.d/rvm.sh ]] && source /etc/profile.d/rvm.sh
 
-cd /u16ruball
+cd /c7ruball
 mv .gemrc $HOME/.gemrc
 
-for file in /u16ruball/version/*;
+for file in /c7ruball/version/*;
 do
   $file
 done
 
-echo "================= Cleaning package lists ==================="
-apt-get clean
-apt-get autoclean
-apt-get autoremove
+#echo "================= Cleaning package lists ==================="
+#apt-get clean
+#apt-get autoclean
+#apt-get autoremove
